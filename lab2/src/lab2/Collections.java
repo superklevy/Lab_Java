b2527b167c46ecca53491bc20e9c2d9ddefba714
superklevy <superklevy@gmil.com>
2018-05-29 20:15:22 +0300

    lab

--- lab2/src/lab2/Collections.java
+++ lab2/src/lab2/Collections.java
@@ -9,22 +9,37 @@ public class Collections {
 	public static void main(String[] args) {
 		List<String> l = new ArrayList<String>();
 		List<String> x = new java.util.ArrayList<String>();
+		
 		l.add("first");
 		l.add("second");
 		l.add("third");
 		l.add("first");
 		
-	
-	    System.out.println(l.toString());
+		x.add("first");
+		x.add("second");
+		x.add("third");
+		x.add("first");
 		
-		l.remove(2);
 	    System.out.println(l.toString());
+	    System.out.println(x.toString());
 	    
+		l.remove(2);
+		x.remove(2);
+		
+	    System.out.println(l.toString());  
 	    System.out.println(l.size());
 	    System.out.println(l.get(0));
 	    System.out.println(l.contains("first"));
 	    System.out.println(l.contains("x"));
+	    
+	    System.out.println(x.toString());  
+	    System.out.println(x.size());
+	    System.out.println(x.get(0));
+	    System.out.println(x.contains("first"));
+	    System.out.println(x.contains("x"));
 		
 	}
+	
+	
 
 }
